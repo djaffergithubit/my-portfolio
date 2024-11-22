f16322a0e0ae7a56ae2b57f47142b7f4b6ad8e4a
Djaffar <d_tadjer@estin.dz>
2024-11-22 21:31:55 +0100

    first-commit

---
+++ src/utils/projects.js
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    title: "Learning Management System Platform",
+    description:
+      "A fullstack web app that helps students find Courses online.",
+    imagePath: "/LMS-Platform.png",
+    link: "",
+    github: "https://github.com/djaffergithubit/LMS-platform-mern-app",
+    isLiked: false,
+  },
+  {
+    title: "Social Clone App",
+    description:
+      "A social media clone app that allows users to share their posts and chat with each other using React, Express and socket.io.",
+    imagePath: "/Social.png",
+    link: "",
+    github: "https://github.com/djaffergithubit/social-mern-app",
+    isLiked: false,
+  },
+  {
+    title: "E-commerce Web App",
+    description:
+      "A web app that allows users to buy products online. built with React, Node.js, Express, and MongoDB.",
+    imagePath: "/e-commerce.png",
+    link: "",
+    github: "https://github.com/djaffergithubit/e-commerce-web-app",
+    isLiked: false,
+  },
+  {
+    title: "Task Management Web App",
+    description:
+      "Allows users to manage their tasks.",
+    imagePath: "/tasks.png",
+    link: "",
+    github: "https://github.com/djaffergithubit/task-management-app",
+    isLiked: false,
+  },
+  {
+    title: "Cnep-tpe-management",
+    description:
+      "A Tkinter-based desktop app that helps manage the TPEs of the CNEP bank.",
+    imagePath: "/tpe.png",
+    link: "",
+    github: "https://github.com/djaffergithubit/cnep-tpe-management-app",
+    isLiked: false,
+  },
+  {
+    title: "Djaffar's Portfolio Website",
+    description:
+      "This is my portfolio website. The project is built with React, and Tailwind CSS and framer motion",
+    imagePath: "/portfolio.png",
+    link: "",
+    github:
+      "https://github.com/djaffergithubit/my-portfolio",
+    isLiked: false,
+  },
+];
---
+++ src/utils/skills.js
@@ -0,0 +1,13 @@
+export const skills = [
+  { name: "React", level: 80 },
+  { name: "Python", level: 80 },
+  { name: "Express", level: 80 },
+  { name: "MySql", level: 80 },
+  { name: "MongoDB", level: 80 },
+  { name: "Django", level: 90 },
+  { name:"Nodejs", level: 70 },
+  { name: "Git && Github", level: 80 },
+  { name: "Redux", level: 60 },
+  { name: "Docker", level: 60 },
+  { name: "Tailwind", level: 50 },
+];
